1c3e8dd4866974d87dad99ebedd1e711f659d50b
Coder731 <stephenobrien1@gmail.com>
2022-08-06 10:40:15 +0000

    DEBUG set to False

--- portfolio_project_5_v4/settings.py
+++ portfolio_project_5_v4/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['portfolio-project-5-v4.herokuapp.com', 'localhost']
 
